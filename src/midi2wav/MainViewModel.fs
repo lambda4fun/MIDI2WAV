9a8ec1bac3edb2e2a3c8618df6ae4fc83afd2ee1
lambda4fun <lambda4fun@users.noreply.github.com>
2017-05-12 05:44:25 +0900

    Add project files.

---
+++ src/midi2wav/MainViewModel.fs
@@ -0,0 +1,8 @@
+﻿namespace ViewModels
+
+open System.Windows
+
+type MainViewModel(window : Window) =
+    do
+        window.MouseLeftButtonDown.AddHandler(fun _ _ ->
+            MessageBox.Show("MouseLeftButtonDown") |> ignore)
