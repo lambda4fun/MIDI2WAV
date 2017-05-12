d1fb9dd645e91fd43fe479faf0b76ffcb06e9567
lambda4fun <lambda4fun@users.noreply.github.com>
2017-05-12 16:11:37 +0900

    Open MIDI file and show its data on DataGrid

--- src/midi-editor/MainViewModel.fs
+++ src/midi-editor/MainViewModel.fs
@@ -4,9 +4,26 @@ open System
 open System.Windows
 open Microsoft.Win32
 open Microsoft.FSharp.Linq.NullableOperators
+open NAudio.Midi
 open CommandUtils
 
+type EventRow = {
+    Index : int
+    AbsoluteTime : int64
+    DeltaTime : int
+    Channel : int
+    CommandCode : MidiCommandCode
+}
+
 type MainViewModel(window : Window) =
+    let loadMidiFile path =
+        let midiFile = NAudio.Midi.MidiFile(path)
+        let track = 0
+        let events = midiFile.Events.[track]
+        let rows = events |> Seq.mapi (fun i e ->
+            { Index=i; AbsoluteTime=e.AbsoluteTime; DeltaTime=e.DeltaTime; Channel=e.Channel; CommandCode=e.CommandCode })
+        window.DataContext <- rows
+
     do
         window.MouseLeftButtonDown.AddHandler(fun _ _ ->
             MessageBox.Show("Greeting from midi-editor!") |> ignore)
@@ -15,7 +32,7 @@ type MainViewModel(window : Window) =
         let dialog = OpenFileDialog(Filter = "MIDI Files|*.mid;*.midi|All Files|*.*")
         let result = dialog.ShowDialog()
         if result ?= true then
-            MessageBox.Show(dialog.FileName) |> ignore)
+            loadMidiFile dialog.FileName)
 
     member __.ExitCommand = functionCommand(fun () ->
         let result = MessageBox.Show("Do you really want to exit?", "", MessageBoxButton.YesNo)
