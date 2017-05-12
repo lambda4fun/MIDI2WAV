47e746469ec618a0beef2e09f6784b09b4d1e64f
lambda4fun <lambda4fun@users.noreply.github.com>
2017-05-12 15:12:37 +0900

    Simplifies the command creations

---
+++ src/midi-editor/CommandUtils.fs
@@ -0,0 +1,11 @@
+﻿module CommandUtils
+
+open System
+open System.Windows.Input
+
+let functionCommand (f : unit -> 'a) = 
+    { new ICommand with
+        [<CLIEvent>]
+        member __.CanExecuteChanged = Event<EventHandler, EventArgs>().Publish
+        member __.CanExecute _ = true
+        member __.Execute _ = f() |> ignore }
--- src/midi-editor/MainViewModel.fs
+++ src/midi-editor/MainViewModel.fs
@@ -1,41 +1,27 @@
 ﻿namespace ViewModels
 
-open System.Windows
 open System
-open System.Windows.Input
+open System.Windows
 open Microsoft.Win32
 open Microsoft.FSharp.Linq.NullableOperators
-
-type OpenCommand() =
-    interface ICommand with
-        [<CLIEvent>]
-        member __.CanExecuteChanged = Event<EventHandler, EventArgs>().Publish
-        member __.CanExecute _ = true
-        member __.Execute _ =
-            let dialog = OpenFileDialog(Filter = "MIDI Files|*.mid;*.midi|All Files|*.*")
-            let result = dialog.ShowDialog()
-            if result ?= true then
-                MessageBox.Show(dialog.FileName) |> ignore
-            ()
-
-type FunctionCommand(execute : unit -> unit) =
-    interface ICommand with
-        [<CLIEvent>]
-        member __.CanExecuteChanged = Event<EventHandler, EventArgs>().Publish
-        member __.CanExecute _ = true
-        member __.Execute _ = execute()
+open CommandUtils
 
 type MainViewModel(window : Window) =
     do
         window.MouseLeftButtonDown.AddHandler(fun _ _ ->
             MessageBox.Show("Greeting from midi-editor!") |> ignore)
 
-    member __.OpenCommand = OpenCommand()
+    member __.OpenCommand = functionCommand(fun () ->
+        let dialog = OpenFileDialog(Filter = "MIDI Files|*.mid;*.midi|All Files|*.*")
+        let result = dialog.ShowDialog()
+        if result ?= true then
+            MessageBox.Show(dialog.FileName) |> ignore)
 
-    member __.ExitCommand = FunctionCommand(fun () ->
+    member __.ExitCommand = functionCommand(fun () ->
         let result = MessageBox.Show("Do you really want to exit?", "", MessageBoxButton.YesNo)
         if result = MessageBoxResult.Yes then
             Application.Current.Shutdown())
-
-    member __.AboutCommand = FunctionCommand(fun () ->
-        MessageBox.Show("MIDI File Editor", "About") |> ignore)
+    
+    member __.AboutCommand = functionCommand(fun () ->
+        MessageBox.Show("MIDI File Editor", "About"))
+        
