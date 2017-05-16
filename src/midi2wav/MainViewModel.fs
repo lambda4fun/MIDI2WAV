b6b426b5d863e78fbfd2c006cfd2704a8478d393
lambda4fun <lambda4fun@users.noreply.github.com>
2017-05-16 21:15:06 +0900

    Implement InputFilePath property

--- src/midi2wav/MainViewModel.fs
+++ src/midi2wav/MainViewModel.fs
@@ -1,8 +1,32 @@
 ﻿namespace ViewModels
 
-open System.Windows
+open Microsoft.Win32
+open Microsoft.FSharp.Linq.NullableOperators
+open CommandUtils
+open Views
+open System.ComponentModel
 
-type MainViewModel(window : Window) =
-    do
-        window.MouseLeftButtonDown.AddHandler(fun _ _ ->
-            MessageBox.Show("MouseLeftButtonDown") |> ignore)
+type ViewModelBase() =
+    let propertyChangedEvent = Event<_, _>()
+    
+    interface INotifyPropertyChanged with
+        [<CLIEvent>]
+        member this.PropertyChanged = propertyChangedEvent.Publish
+    
+    member this.NotifyPropertyChanged(name : string) =
+        propertyChangedEvent.Trigger(this, PropertyChangedEventArgs(name))
+
+type MainViewModel(window : MainWindow) as self =
+    inherit ViewModelBase()
+
+    let mutable inputFilePath = ""
+
+    member __.InputFilePath with get() = inputFilePath
+                            and  set v = inputFilePath <- v
+                                         self.NotifyPropertyChanged("InputFilePath")
+
+    member __.BrowseInputFileCommand = functionCommand(fun () ->
+        let dialog = OpenFileDialog()
+        let result = dialog.ShowDialog(window)
+        if result ?= true then
+            self.InputFilePath <- dialog.FileName)
