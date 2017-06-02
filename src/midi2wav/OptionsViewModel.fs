ad16754b0f330e0caa9cd518f25144c43b4d6f62
lambda4fun <lambda4fun@users.noreply.github.com>
2017-06-02 11:09:51 +0900

    Add "SoundFontPath" to settings

--- src/midi2wav/OptionsViewModel.fs
+++ src/midi2wav/OptionsViewModel.fs
@@ -1,6 +1,7 @@
 ﻿namespace ViewModels
 
 open System
+open System.Configuration
 open Microsoft.Win32
 open Microsoft.FSharp.Linq.NullableOperators
 open CommandUtils
@@ -10,6 +11,11 @@ type OptionsViewModel(view : OptionsDialog) as self =
     inherit ViewModelBase()
 
     let mutable soundFontPath = ""
+    let config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None)
+
+    do
+        view.Loaded.AddHandler(fun _ _ ->
+            self.SoundFontPath <- config.AppSettings.Settings.["SoundFontPath"].Value)
 
     member __.SoundFontPath with get() = soundFontPath
                             and  set v = soundFontPath <- v
@@ -22,6 +28,8 @@ type OptionsViewModel(view : OptionsDialog) as self =
             self.SoundFontPath <- dialog.FileName)
     
     member __.OkCommand = functionCommand(fun () ->
+        config.AppSettings.Settings.["SoundFontPath"].Value <- soundFontPath
+        config.Save()
         view.DialogResult <- Nullable(true))
 
     member __.CancelCommand = functionCommand(fun () ->
