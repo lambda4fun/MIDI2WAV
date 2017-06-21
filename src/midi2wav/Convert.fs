3ced4d8791aa7a9f29500b23afc7089d8b4bea7a
lambda4fun <lambda4fun@users.noreply.github.com>
2017-06-21 12:52:56 +0900

    Rewrite the convert module

--- src/midi2wav/Convert.fs
+++ src/midi2wav/Convert.fs
@@ -18,129 +18,149 @@ type beat
 type tick
 
 [<Measure>]
-type min
+type sample
 
-let tryFindGeneratorByType t (generators : Generator[]) =
-    generators
-    |> Seq.tryFind (fun g -> g.GeneratorType = t)
-
-let findGeneratorsByKey key (instrument : Instrument) =
-    let find (gs : Generator[]) =
-        let keyRangeOption = gs |> tryFindGeneratorByType GeneratorEnum.KeyRange
-        let sampleIdOption = gs |> tryFindGeneratorByType GeneratorEnum.SampleID
-        let check (keyRange : Generator) (sampleId : Generator) =
-            if keyRange.LowByteAmount <= key && key <= keyRange.HighByteAmount
-            then
-                Some (sampleId.SampleHeader, (keyRange.LowByteAmount, keyRange.HighByteAmount))
-            else
-                None
-        
-        Option.map2 check keyRangeOption sampleIdOption
-        |> Option.bind id
-    
-    instrument.Zones
-    |> Seq.map (fun zone -> zone.Generators)
-    |> Seq.tryPick find
-
-let findSampleHeader key (instrument : Instrument) =
-    instrument
-    |> findGeneratorsByKey key
-    |> Option.map fst
+type Sample = {
+    PreLoop  : int16[]
+    Loop     : int16[]
+    PostLoop : int16[] }
 
 let frequencies = [|
-    8.17579891564368; 8.66195721802723; 9.17702399741896; 9.722718241315; 10.3008611535272; 10.9133822322813; 11.5623257097385; 12.2498573744296; 12.9782717993733;
-    13.75; 14.5676175474403; 15.4338531642538; 16.3515978312874; 17.3239144360545; 18.3540479948379; 19.44543648263; 20.6017223070543; 21.8267644645627; 23.1246514194771; 24.4997147488593; 25.9565435987465;
-    27.5; 29.1352350948806; 30.8677063285077; 32.7031956625748; 34.6478288721089; 36.7080959896759; 38.89087296526; 41.2034446141087; 43.6535289291254; 46.2493028389542; 48.9994294977186; 51.9130871974931;
-    55.0; 58.2704701897612; 61.7354126570154; 65.4063913251496; 69.2956577442179; 73.4161919793518; 77.7817459305201; 82.4068892282174; 87.3070578582509; 92.4986056779085; 97.9988589954372; 103.826174394986; 
-    110.0; 116.540940379522; 123.470825314031; 130.812782650299; 138.591315488436; 146.832383958704; 155.56349186104; 164.813778456435; 174.614115716502; 184.997211355817; 195.997717990875; 207.652348789972;
-    220.0; 233.081880759045; 246.941650628062; 261.625565300599; 277.182630976872; 293.664767917407; 311.126983722081; 329.62755691287; 349.228231433004; 369.994422711634; 391.995435981749; 415.304697579945;
-    440.0; 466.16376151809; 493.883301256124; 523.251130601197; 554.365261953744; 587.329535834815; 622.253967444162; 659.25511382574; 698.456462866008; 739.988845423269; 783.990871963499; 830.609395159891;
-    880.0; 932.32752303618; 987.766602512249; 1046.5022612024; 1108.73052390749; 1174.65907166963; 1244.50793488832; 1318.51022765148; 1396.91292573202; 1479.97769084654; 1567.981743927; 1661.21879031978;
-    1760.0; 1864.65504607236; 1975.5332050245; 2093.00452240479; 2217.46104781498; 2349.31814333926; 2489.01586977665; 2637.02045530296; 2793.82585146403; 2959.95538169308; 3135.963487854; 3322.43758063957;
-    3520.0; 3729.31009214472; 3951.066410049; 4186.00904480959; 4434.92209562996; 4698.63628667853; 4978.0317395533; 5274.04091060593; 5587.65170292807; 5919.91076338616; 6271.926975708; 6644.87516127914;
-    7040.0; 7458.62018428945; 7902.132820098; 8372.01808961917; 8869.84419125993; 9397.27257335706; 9956.06347910661; 10548.0818212119; 11175.3034058562; 11839.8215267723; 12543.853951416 |] |> Array.AsReadOnly
+    8.175<Hz>; 8.661<Hz>; 9.177<Hz>; 9.722<Hz>; 10.300<Hz>; 10.913<Hz>; 11.562<Hz>; 12.249<Hz>; 12.978<Hz>;
+    13.75<Hz>; 14.567<Hz>; 15.433<Hz>; 16.351<Hz>; 17.323<Hz>; 18.354<Hz>; 19.445<Hz>; 20.601<Hz>; 21.826<Hz>; 23.124<Hz>; 24.499<Hz>; 25.956<Hz>;
+    27.5<Hz>; 29.135<Hz>; 30.867<Hz>; 32.703<Hz>; 34.647<Hz>; 36.708<Hz>; 38.890<Hz>; 41.203<Hz>; 43.653<Hz>; 46.249<Hz>; 48.999<Hz>; 51.913<Hz>;
+    55.0<Hz>; 58.270<Hz>; 61.735<Hz>; 65.406<Hz>; 69.295<Hz>; 73.416<Hz>; 77.781<Hz>; 82.406<Hz>; 87.307<Hz>; 92.498<Hz>; 97.998<Hz>; 103.826<Hz>; 
+    110.0<Hz>; 116.540<Hz>; 123.470<Hz>; 130.812<Hz>; 138.591<Hz>; 146.832<Hz>; 155.563<Hz>; 164.813<Hz>; 174.614<Hz>; 184.997<Hz>; 195.997<Hz>; 207.652<Hz>;
+    220.0<Hz>; 233.081<Hz>; 246.941<Hz>; 261.625<Hz>; 277.182<Hz>; 293.664<Hz>; 311.126<Hz>; 329.627<Hz>; 349.228<Hz>; 369.994<Hz>; 391.995<Hz>; 415.304<Hz>;
+    440.0<Hz>; 466.163<Hz>; 493.883<Hz>; 523.251<Hz>; 554.365<Hz>; 587.329<Hz>; 622.253<Hz>; 659.255<Hz>; 698.456<Hz>; 739.988<Hz>; 783.990<Hz>; 830.609<Hz>;
+    880.0<Hz>; 932.327<Hz>; 987.766<Hz>; 1046.502<Hz>; 1108.730<Hz>; 1174.659<Hz>; 1244.507<Hz>; 1318.510<Hz>; 1396.912<Hz>; 1479.977<Hz>; 1567.981<Hz>; 1661.218<Hz>;
+    1760.0<Hz>; 1864.655<Hz>; 1975.533<Hz>; 2093.004<Hz>; 2217.461<Hz>; 2349.318<Hz>; 2489.015<Hz>; 2637.020<Hz>; 2793.825<Hz>; 2959.955<Hz>; 3135.963<Hz>; 3322.437<Hz>;
+    3520.0<Hz>; 3729.310<Hz>; 3951.066<Hz>; 4186.009<Hz>; 4434.922<Hz>; 4698.636<Hz>; 4978.031<Hz>; 5274.040<Hz>; 5587.651<Hz>; 5919.910<Hz>; 6271.926<Hz>; 6644.875<Hz>;
+    7040.0<Hz>; 7458.620<Hz>; 7902.132<Hz>; 8372.018<Hz>; 8869.844<Hz>; 9397.272<Hz>; 9956.063<Hz>; 10548.081<Hz>; 11175.303<Hz>; 11839.821<Hz>; 12543.853<Hz> |] |> Array.AsReadOnly
 
-let noteToFrequency note = frequencies.[note]
+let keyToFrequency key = frequencies.[key]
 
-// TODO: Consider using BufferedWaveProvider or MultiplexingWaveProvider.
-type SoundFontWaveProvider(soundFont : SoundFont, instrument : Instrument) as this =
-    inherit WaveProvider16()
-    
-    let mutable key = 60
-    // TODO: Process Option.get error.
-    let mutable h = findSampleHeader (byte key) instrument |> Option.get
-    let mutable startKey = 60
-
-    let changeKey k =
-        key <- k
-        findGeneratorsByKey (byte key) instrument
-        |> Option.iter (fun (sampleHeader, (sk, _)) ->
-            h <- sampleHeader
-            startKey <- int sk)
-        // TODO: Use Result discriminated union instead of exception.
-        let currentSampleRate = (this :> WaveProvider16).WaveFormat.SampleRate
-        if int h.SampleRate <> currentSampleRate
-        then failwithf "Changing sample rate after initialization is not supported. (%dHz -> %dHz)" currentSampleRate h.SampleRate
-
-    do
-        (this :> WaveProvider16).SetWaveFormat(int h.SampleRate, 1)
-    
-    member __.Key with get() = key
-                  and  set k = changeKey k
-        
-    override __.Read(buffer : int16[], offset : int, sampleCount : int) =
-        let mutable sampleIndex = 0u
-        let deltaIndex = float (noteToFrequency startKey) / float (noteToFrequency this.Key)
-
-        // TODO: Shift sampleIndex instead of i so that buffer is filled correctly.
-        { 0.0 .. deltaIndex .. (float sampleCount - 1.0) }
-        |> Seq.iter (fun i ->
-            let sample = BitConverter.ToInt16(soundFont.SampleData, int (h.Start + sampleIndex) * 2)
-            if BitConverter.IsLittleEndian
-            then buffer.[int i] <- sample
-            else buffer.[int i] <- (sample <<< 8) ||| (sample >>> 8)
-
-            if sampleIndex >= h.EndLoop - h.Start && uint32 i < uint32 sampleCount - (h.End - h.EndLoop)
-            then sampleIndex <- h.StartLoop - h.Start
-            else sampleIndex <- sampleIndex + 1u)
-        sampleCount
-
-let convertMidiToWave (soundFontPath : string) midiPath wavePath =
-    let soundFont = SoundFont(soundFontPath)
-    let instrumentName = "Yamaha Grand Piano"
-    let instrument = soundFont.Instruments
-                     |> Seq.find (fun i -> i.Name = instrumentName)
-    let waveProvider = SoundFontWaveProvider(soundFont, instrument)
-
-    let midiFile = NAudio.Midi.MidiFile(midiPath)
+let convertMidiToWave (soundFontPath : string) (instrumentName : string) (midiFilePath : string) (waveFilePath : string) =
+    let midiFile = MidiFile(midiFilePath)
     let track = 0
     let events = midiFile.Events.[track]
     let tempo = 0.5<s/beat> // 500,000 microseconds
     let ticksPerBeat = float midiFile.DeltaTicksPerQuarterNote * 1.0<tick/beat>
 
-    use recorder = new WaveRecorder(waveProvider, wavePath)
-    let buffer = ref <| Array.zeroCreate 1024
+    let soundFont = SoundFont(soundFontPath)
+    let instrument = soundFont.Instruments |> Seq.find (fun i -> i.Name = instrumentName)
+    let sampleRate = 32000<Hz>
+    let numChannels = 1
+    
+    use writer = new WaveFileWriter(waveFilePath, WaveFormat(int sampleRate, numChannels))
+    
+    let writeSamples (samples : int16[]) (count : int<sample>) =
+        writer.WriteSamples(samples, 0, int count)
 
-    let recode (event1 : NoteOnEvent) ticks =
-        let noteLength = ticks * tempo / ticksPerBeat
-        let sampleCount = int (noteLength * (float waveProvider.WaveFormat.SampleRate * 1.0<Hz>))
-        let byteCount = sampleCount * 2
-        Array.Resize(buffer, byteCount)
+    let findZoneByKey key =
+        instrument.Zones
+        |> Array.filter (fun zone ->
+            zone.Generators
+            |> Array.exists (fun gen -> gen.GeneratorType = GeneratorEnum.SampleID))
+        |> Array.find (fun zone ->
+            zone.Generators
+            |> Array.exists (fun gen ->
+                gen.GeneratorType = GeneratorEnum.KeyRange &&
+                int gen.LowByteAmount <= key && key <= int gen.HighByteAmount))
 
-        waveProvider.Key <- event1.NoteNumber
-        recorder.Read(!buffer, 0, byteCount) |> ignore
+    let ticksToSamples (ticks : int<tick>) =
+        let noteLength = float ticks * tempo / ticksPerBeat
+        int (noteLength * float sampleRate) * 1<sample>
+        
+    let recode (event : NoteOnEvent) ticks =
+        let sampleCount = ticksToSamples ticks
+        let key = event.NoteNumber
+        let zone = findZoneByKey key
+        let generators = zone.Generators
+        let sampleId = generators |> Array.find (fun gen -> gen.GeneratorType = GeneratorEnum.SampleID)
+        let sampleHeader = sampleId.SampleHeader
+        let mutable startIndex     = int sampleHeader.Start
+        let mutable startLoopIndex = int sampleHeader.StartLoop
+        let mutable endLoopIndex   = int sampleHeader.EndLoop
+        let mutable endIndex       = int sampleHeader.End
+        let mutable rootKey = int sampleHeader.OriginalPitch
+
+        for gen in generators do
+            match gen.GeneratorType with
+            | GeneratorEnum.OverridingRootKey ->
+                if 0s <= gen.Int16Amount && gen.Int16Amount <= 127s then
+                    rootKey <- int gen.Int16Amount
+            // TODO: modify startIndex and endIndex
+            | GeneratorEnum.StartAddressCoarseOffset -> ()
+            | GeneratorEnum.StartAddressOffset -> ()
+            | GeneratorEnum.StartLoopAddressCoarseOffset -> ()
+            | GeneratorEnum.StartLoopAddressOffset -> ()
+            | GeneratorEnum.EndAddressCoarseOffset -> ()
+            | GeneratorEnum.EndAddressOffset -> ()
+            | GeneratorEnum.EndLoopAddressCoarseOffset -> ()
+            | GeneratorEnum.EndLoopAddressOffset -> ()
+            | _ -> ()
+        
+        let frequency = keyToFrequency key
+        let rootFrequency = keyToFrequency rootKey
+        let delta = frequency / rootFrequency
+        
+        let toInt16s (bytes : byte[]) =
+            bytes
+            |> Seq.chunkBySize 2
+            |> Seq.map (fun pair -> BitConverter.ToInt16(pair, 0))
+            |> Seq.toArray
+        
+        let interpolate (src : int16[]) delta =
+            let dst = Array.zeroCreate (int (float src.Length / delta))
+            ({ 0 .. dst.Length-1 }, { 0.0 .. delta .. float (src.Length-1) })
+            ||> Seq.iter2 (fun dstIndex srcIndex ->
+                let r1 = srcIndex % 1.0
+                let r2 = 1.0 - r1
+                let src = (float src.[int srcIndex] * r1) + (float src.[int (ceil srcIndex)] * r2)
+                dst.[dstIndex] <- int16 src)
+            dst
+            
+        let sample = {
+            PreLoop  = interpolate (toInt16s soundFont.SampleData.[startIndex*2 .. (startLoopIndex*2)-1]) delta
+            Loop     = interpolate (toInt16s soundFont.SampleData.[startLoopIndex*2 .. (endLoopIndex*2)+1]) delta
+            PostLoop = interpolate (toInt16s soundFont.SampleData.[(endLoopIndex+1)*2 .. (endIndex*2)+1]) delta }
+        
+        let preLoopLength  = min sampleCount (sample.PreLoop.Length * 1<sample>)
+        let postLoopLength = max 0<sample> (min (sampleCount - preLoopLength) (sample.PostLoop.Length * 1<sample>))
+        let loopLength     = max 0<sample> (min (sampleCount- preLoopLength - postLoopLength) (sample.Loop.Length * 1<sample>))
+
+        writeSamples sample.PreLoop preLoopLength
+
+        if loopLength > 0<sample> then
+            for _ in 1 .. (sampleCount / loopLength) do
+                writeSamples sample.Loop loopLength
+            writeSamples sample.Loop (sampleCount % loopLength)
+
+        writeSamples sample.PostLoop postLoopLength
+
+        printfn "write: note = %s (%d), duration = %d (%d ticks = %d samples)" event.NoteName event.NoteNumber event.NoteLength ticks sampleCount
 
     let noteOnEvents =
         events
         |> Seq.filter (fun event -> event.CommandCode = MidiCommandCode.NoteOn)
         |> Seq.map (fun event -> event :?> NoteOnEvent)
 
+    let skip (n : int<sample>) =
+        writeSamples (Array.zeroCreate<int16> (int n)) n
+
+    let recodeNoteOn (event : NoteOnEvent) length =
+        let noteOnLength = event.NoteLength * 1<tick>
+        recode event noteOnLength
+        skip (ticksToSamples (length - noteOnLength))
+
     noteOnEvents
     |> Seq.pairwise
     |> Seq.iter (fun (event1, event2) ->
-        let ticks = float (event2.AbsoluteTime - event1.AbsoluteTime) * 1.0<tick>
-        recode event1 ticks)
+        let noteLength = int (event2.AbsoluteTime - event1.AbsoluteTime) * 1<tick>
+        recodeNoteOn event1 noteLength)
+
     let lastMidiEvent = Seq.last events
     let lastNoteOnEvent = Seq.last noteOnEvents
-    let ticks = float (lastMidiEvent.AbsoluteTime - lastNoteOnEvent.AbsoluteTime) * 1.0<tick>
-    recode lastNoteOnEvent ticks
+    let ticks = int (lastMidiEvent.AbsoluteTime - lastNoteOnEvent.AbsoluteTime) * 1<tick>
+    recodeNoteOn lastNoteOnEvent ticks
