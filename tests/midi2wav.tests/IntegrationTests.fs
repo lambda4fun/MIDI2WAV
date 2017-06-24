412e4f0566b9f92691363a0ed9c07f38fa12239f
lambda4fun <lambda4fun@users.noreply.github.com>
2017-06-24 19:35:58 +0900

    Add integration tests

---
+++ tests/midi2wav.tests/IntegrationTests.fs
@@ -0,0 +1,12 @@
+﻿open System
+open Convert
+
+[<EntryPoint>]
+let main argv =
+    let convert = convertMidiToWave "FluidR3_GM.sf2" "Yamaha Grand Piano"
+    convert "../../../../resources/3midi.mid" "3midi-new.wav"
+    convert "../../../../resources/12midi.mid" "12midi-new.wav"
+
+    printfn "Press any key to exit..."
+    Console.ReadKey(intercept = true) |> ignore
+    0
