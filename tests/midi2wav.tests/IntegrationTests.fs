f5ee5cb6e2a4fb951871f43b17f3ed8b919ef0e2
lambda4fun <lambda4fun@users.noreply.github.com>
2017-06-28 22:00:44 +0900

    Extract convert test

--- tests/midi2wav.tests/IntegrationTests.fs
+++ tests/midi2wav.tests/IntegrationTests.fs
@@ -1,11 +1,14 @@
 ﻿open System
 open Convert
 
+let convertTest() =
+    let convert = convertMidiToWave "FluidR3_GM.sf2"
+    do convert "../../../../resources/3midi.mid" "3midi-new.wav"
+    do convert "../../../../resources/12midi.mid" "12midi-new.wav"
+
 [<EntryPoint>]
 let main argv =
-    let convert = convertMidiToWave "FluidR3_GM.sf2" "Yamaha Grand Piano"
-    convert "../../../../resources/3midi.mid" "3midi-new.wav"
-    convert "../../../../resources/12midi.mid" "12midi-new.wav"
+    convertTest()
 
     printfn "Press any key to exit..."
     Console.ReadKey(intercept = true) |> ignore
